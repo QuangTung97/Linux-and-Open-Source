9a4355c58531857d315e6504a2de7bedf10625e3
quangtung97 <quangtung29121997@gmail.com>
2017-11-11 13:41:53 +0700

    Fix bug when sin(log 2)

--- main.cpp
+++ main.cpp
@@ -6,8 +6,14 @@
 int main(int argc, char **argv) {
     if (argc < 2) 
         return 1;
-    double x = std::stod(argv[1]);
-    std::cout.precision(std::numeric_limits<double>::max_digits10);
-    std::cout << std::tan(x) << std::endl;
+    try {
+        double x = std::stod(argv[1]);
+    } catch (std::invalid_argument& e) {
+        return 1;
+    } catch (std::out_of_range& e) {
+        return 1;
+    }
+    // std::cout.precision(std::numeric_limits<double>::max_digits10);
+    // std::cout << x << std::endl;
     return 0;
 }
