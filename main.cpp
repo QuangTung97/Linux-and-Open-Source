9174034d836c25af7b413f5c30dc5d3b77bc178b
quangtung97 <quangtung29121997@gmail.com>
2017-12-09 11:52:54 +0700

    Add pi and e

--- main.cpp
+++ main.cpp
@@ -4,16 +4,21 @@
 #include <cmath>
 
 int main(int argc, char **argv) {
+    double x = 0;
     if (argc < 2) 
         return 1;
     try {
-        double x = std::stod(argv[1]);
+        x = std::stod(argv[1]);
     } catch (std::invalid_argument& e) {
         return 1;
     } catch (std::out_of_range& e) {
         return 1;
     }
     // std::cout.precision(std::numeric_limits<double>::max_digits10);
-    // std::cout << x << std::endl;
+    const double epsilon = 1.0e-13;
+    if (std::abs(x) < epsilon)
+        x = 0;
+    std::cout.precision(8);
+    std::cout << x << std::endl;
     return 0;
 }
